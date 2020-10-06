60e8907af3d1145a50786b6133accc78e6a1b6cd
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-10-05 21:33:28 -0600

    primer ejercicio JS

---
+++ 02_kata_Js/01_intro_js/Ejercicio/main.js
@@ -0,0 +1,14 @@
+var mascota1 = "Pedrito";
+var mascota2 = "Rufo";
+var mascota3 = "Chancla";
+
+console.log("Mis mascotas son " + mascota1 + ", " + mascota2 + " y " + mascota3 + ". Les quiero mucho");
+
+var fruta1 = prompt("Dime una fruta");
+var fruta2 = prompt("Dime otra fruta");
+console.log("Hoy compre " + fruta1 + " y " + fruta2 + " en el mercado");
+
+var num = "100";
+console.log("variable num =", num, typeof num);
+num = Number(num);
+console.log("variable num cambio ", num, typeof num);
