856eef948dafa58430b91611bb4a3094dde84484
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-10-06 19:42:09 -0600

    segunda clase de js --> condicionales

---
+++ 02_kata_Js/02_condicionales/main.js
@@ -0,0 +1,50 @@
+// > ---> mayor que
+// < ---> menor que 
+// >= ---> mayor igual
+// <= ---> menor igual
+// = ---> declarar
+// == ---> comparar
+// === ---> compara identicamente
+// !== ---> diferente de
+
+var numero = 10;
+
+if(numero === 10) {
+    console.log("Es verdad");
+} else {
+    console.log("Es falso");
+}
+
+if(numero > 10) {
+    console.log("Es verdad");
+} else {
+    console.log("Es falso");
+}
+
+if(numero < 10) {
+    console.log("Es verdad");
+} else {
+    console.log("Es falso");
+}
+
+if(numero >= 10) {
+    console.log("Es verdad");
+} else {
+    console.log("Es falso");
+}
+
+if(numero <= 10) {
+    console.log("Es verdad");
+} else {
+    console.log("Es falso");
+}
+
+if(numero !== 10) {
+    console.log("Es verdad");
+} else {
+    console.log("Es falso");
+}
+
+// Se puede encontrar de esta manera
+// if (numero === 10) console.log("Es verdad")
+// else console.log("Error")
