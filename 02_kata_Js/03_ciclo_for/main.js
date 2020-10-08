9c095b94af993a5c7ac73d9310b5881e309d9d87
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-10-07 19:16:03 -0600

    clase ciclo for

---
+++ 02_kata_Js/03_ciclo_for/main.js
@@ -0,0 +1,8 @@
+// ----------> Estructura <----------
+// for(inicio; limite; secuencia) {
+//     accion en que mi condicion se cumpla
+// }
+
+for(var i = 0; i <= 10; i++) {
+    console.log(i);
+}
