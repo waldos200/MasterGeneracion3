8b2c563e7f80220255168d0d75c6934cd0b98f72
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-10-12 19:03:40 -0600

    array

---
+++ 02_kata_Js/04_array_object/main.js
@@ -0,0 +1,9 @@
+// ----------- Array o arreglos -----------
+
+// posicion    0        1         2   
+var color = ["Azul", "Verde", "Naranja"];
+
+console.log(color);
+console.log(color[1]);
+console.log(color[2]);
+
