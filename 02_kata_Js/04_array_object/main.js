ac9363e1f15debd22932d84627ec5f2e60fdcca3
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-10-12 19:04:25 -0600

    Metodos

--- 02_kata_Js/04_array_object/main.js
+++ 02_kata_Js/04_array_object/main.js
@@ -7,3 +7,23 @@ console.log(color);
 console.log(color[1]);
 console.log(color[2]);
 
+// ----------- Metodos -----------
+// .push();
+// Empuja un valor en la ultima posicion
+color.push("Negro");
+color.push("Blanco");
+color.push("Rosa");
+
+console.log(color);
+
+// .pop();
+// Eliminar el ultimo valor
+color.pop();
+color.pop();
+
+console.log(color);
+
+// .splice();
+color.splice(2, 0, "Cafe", "Rojo");
+console.log(color);
+
