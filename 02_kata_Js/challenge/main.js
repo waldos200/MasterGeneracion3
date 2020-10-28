bad1b8f7adc73381b47574c000d5b77a7b1fed9b
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-10-27 20:48:17 -0600

    llamando a pokemons

--- 02_kata_Js/challenge/main.js
+++ 02_kata_Js/challenge/main.js
@@ -1,22 +1,4 @@
-import pokedex from './poke';
-
-
-document.write('./poke.js');
-
-
-
-
-mira es otra opcion:
-
-Una forma más bonita sería crear tu propia función sin preocuparte de si se llama de manera síncrona o asíncrona:
-
-function importarScript(nombre) {
-    var s = document.createElement("script");
-    s.src = nombre;
-    document.querySelector("head").appendChild(s);
+function obtenerPokemons(){
+    var pokemons = Pokemon();
+    console.log(pokemons);
 }
-Y llamar a la función cada vez que quieras importar:
-
-importarScript("codigo1.js");
-importarScript("codigo2.js");
-importarScript("codigo3.js");
