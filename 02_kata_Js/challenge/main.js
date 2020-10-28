c1e8709d5dd1db18727f265382a3f88af9cbcf10
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-10-27 21:33:13 -0600

    pintar los elementos de los pokemons

--- 02_kata_Js/challenge/main.js
+++ 02_kata_Js/challenge/main.js
@@ -1,4 +1,15 @@
 function obtenerPokemons(){
-    var pokemons = Pokemon();
-    console.log(pokemons);
+    var pokem = Pokemon();
+
+    var pokeName = document.getElementById('name');
+    pokeName.innerHTML = pokem[0].name.toUpperCase();
+
+    var pokeId = document.getElementById('pkdx_id');
+    pokeId.innerHTML = pokem[0].pkdx_id;
+
+    var pokeImg = document.getElementById('art_url');
+    pokeImg.src = pokem[0].art_url;
+    
+    var pokeType = document.getElementById('types');
+    pokeType.innerHTML = "Tipos: " + pokem[0].types;
 }
