e08964b21d92e530b06fddbe0501f1d278841e34
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-11-11 21:28:48 -0600

    ejercicios

--- 03_kata_utilities/02_ECMA6/main.js
+++ 03_kata_utilities/02_ECMA6/main.js
@@ -39,3 +39,44 @@ const suma = (a, b) => {
     return a + b;
 };
 suma(5, 10);
+
+// Ejemplos
+
+const palindrome = (cadena) => {
+    let array = cadena.split("");
+    let reverse = array.reverse();
+    return cadena == reverse.join("") ? "Si es palindromo" : "no es palindromo";
+}
+console.log(palindrome("ana").toLowerCase());
+
+const paises = ["Mexico", "Panama", "Guatemala", "El Salvador"];
+const longestCountry = (paises) =>{
+    console.log(...paises);
+}
+
+const farenheitToCelsius = (y) =>{
+    return (y - 32) * 5/9;
+}
+console.log(farenheitToCelsius(64));
+
+const ppt = (us1, us2) =>{
+    if (us1 === "piedra" && us2 === "piedra" || us1 === "papel" && us2 === "papel" || us1 === "tijera" && us2 === "tijera") {
+        console.log("Empate");
+    } else if (us1 === "piedra" && us2 == "papel") {
+        console.log("Gana la segunda persona");
+    } else if (us1 === "papel" && us2 === "tijera") {
+        console.log("Gana la segunda persona");
+    } else if (us1 === "tijera" && us2 === "piedra") {
+        console.log("Gana la segunda persona");
+    } else {
+        console.log("Gana la primera persona");
+    }
+}
+
+console.log(ppt("piedra", "papel"));
+
+const num = (x) =>{
+    return x;
+}
+
+console.log(num(5));
