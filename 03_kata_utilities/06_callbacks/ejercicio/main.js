4e740bebecef4eaaf703971282229a6586a7053b
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-11-25 21:14:46 -0600

    agregue el get salario

--- 03_kata_utilities/06_callbacks/ejercicio/main.js
+++ 03_kata_utilities/06_callbacks/ejercicio/main.js
@@ -11,12 +11,25 @@ const getEmployeeById = (id, callback) => {
 };
 
 const getSalarioById = (id, callback) => {
-    // Codigo para traerme el salario
-}
+    const salarioDB = salarios.find(
+        (salario) => salario.id === id
+    );
+    if (!salarioDB) {
+        return callback("No existe informacion de ese ID");
+    }
+    callback(null, salarioDB);
+};
 
 getEmployeeById(3, (error, empleado) => {
     if (error) {
         return console.log("error", error);
     }
     console.log(empleado);
+});
+
+getSalarioById(3, (error, salario) => {
+    if (error) {
+        return console.log("error", error);
+    }
+    console.log(salario);
 });
