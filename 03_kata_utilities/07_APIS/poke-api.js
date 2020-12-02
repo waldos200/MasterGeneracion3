290bef40a204c579cce0e3d00600b2c2f28c0e87
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-12-01 20:57:10 -0600

    seguda parte de poke-api

--- 03_kata_utilities/07_APIS/poke-api.js
+++ 03_kata_utilities/07_APIS/poke-api.js
@@ -9,3 +9,19 @@ request.get("https://pokeapi.co/api/v2/pokemon/ditto", (error, response, body) =
         console.log(`Ocurrio un error en la peticion, ${error}`);
     }
 });
+
+const pedirPokemon = (pokemon) => {
+    request.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`, (error, response, body) =>{
+        if (response.statusCode === 200) {
+            const data = JSON.parse(body);
+            // console.log(data);
+            console.log(`${data.name} 🥰`);
+
+        }else{
+            console.error(`🚫 Ocurrio un error en la peticion 🚫 ${error}`);
+        }
+    });
+}
+
+pedirPokemon('ditto');
+pedirPokemon(500)
