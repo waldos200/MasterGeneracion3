05c3f77ef3774ed459c4a64c25e6a80f262c6d83
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-12-02 20:23:52 -0600

    creando un autor

--- 03_kata_utilities/08_HTTP/client.js
+++ 03_kata_utilities/08_HTTP/client.js
@@ -6,17 +6,17 @@ const URL_BASE = "https://goodreads-devf-aaron.herokuapp.com"
 
 // -> /api/v1/authors
 const getAllAuthors = () => {
-    request.get(`${URL_BASE}/api/v1/authors/`, (err, res, body) =>{
+    request.get(`${URL_BASE}/api/v1/authors/`, (err, res, body) => {
         if (res.statusCode === 200) {
             const data = JSON.parse(body);
             console.log(data);
-        }else{
+        } else {
             console.log(err);
         }
     });
 }
 
-getAllAuthors();
+// getAllAuthors();
 
 // ------------> CRUD <------------
 // CREATE
@@ -24,3 +24,27 @@ getAllAuthors();
 // UPDATE
 // DELETE
 
+const createAuthor = (name, last_name, nacionalidad, biography, gender, age, isAlive) => {
+    const URL_POST = `${URL_BASE}/api/v1/authors/`;
+    const jsonSend = {
+        "name": name,
+        "last_name": last_name,
+        "nacionalidad": nacionalidad,
+        "biography": biography,
+        "gender": gender,
+        "age": age,
+        "is_alive": isAlive
+    }
+
+    request.post(URL_POST, {form: jsonSend}, (err, res, body) => {
+        if(res.statusCode === 201){
+            const newAuthor = JSON.parse(body);
+            console.log(newAuthor);
+            return newAuthor
+        } else {
+            console.log(err);
+        }
+    })
+}
+
+// createAuthor("Juan", "Guarnizo", "MX", "Streamer", "M", 22, true)
