374a6ccd46300cbfd34897f5f31f48df2ff8dcbf
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-12-02 20:49:47 -0600

    codigo para hacer un update a un usuario

--- 03_kata_utilities/08_HTTP/client.js
+++ 03_kata_utilities/08_HTTP/client.js
@@ -5,6 +5,7 @@ const URL_BASE = "https://goodreads-devf-aaron.herokuapp.com"
 // end-points
 
 // -> /api/v1/authors
+// READ
 const getAllAuthors = () => {
     request.get(`${URL_BASE}/api/v1/authors/`, (err, res, body) => {
         if (res.statusCode === 200) {
@@ -24,6 +25,7 @@ const getAllAuthors = () => {
 // UPDATE
 // DELETE
 
+// CREATE
 const createAuthor = (name, last_name, nacionalidad, biography, gender, age, isAlive) => {
     const URL_POST = `${URL_BASE}/api/v1/authors/`;
     const jsonSend = {
@@ -49,3 +51,28 @@ const createAuthor = (name, last_name, nacionalidad, biography, gender, age, isA
 
 // createAuthor("Juan", "Guarnizo", "MX", "Streamer", "M", 22, true)
 // 13479
+
+// UPDATE
+const updateAuthor = (name, last_name, nacionalidad, biography, gender, age, isAlive, id) => {
+    const URL_PUT = `${URL_BASE}/api/v1/authors/${id}/`;
+    const jsonSend = {
+        "name": name,
+        "last_name": last_name,
+        "nacionalidad": nacionalidad,
+        "biography": biography,
+        "gender": gender,
+        "age": age,
+        "is_alive": isAlive
+    }
+
+    request.put(URL_PUT, {form : jsonSend}, (err, res, body) => {
+        if(res.statusCode === 200){
+            const upAuthor = JSON.parse(body);
+            console.log(upAuthor);
+        } else {
+            console.log(err);
+        }
+    })
+}
+
+// updateAuthor("Angel", "Tengo hambre, ya comi", "ES", "Soy tu padre", "F", 10, false, 12854);
