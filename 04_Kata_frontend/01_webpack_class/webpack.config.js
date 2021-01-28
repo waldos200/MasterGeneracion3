afb449f7de2a001ae6e78eb03da25a174277011f
Waldos Mendoza <mendozawaldos@hotmail.com>
2021-01-27 21:37:55 -0600

    configuramos para que pueda leer el html

--- 04_Kata_frontend/01_webpack_class/webpack.config.js
+++ 04_Kata_frontend/01_webpack_class/webpack.config.js
@@ -1,3 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    
+    module: {
+        rules:[ // En "rules" se cargan los loaders
+            // loader de html
+            {
+                test: /\.html$/, // --> que archivo voy a buscar
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
 }
