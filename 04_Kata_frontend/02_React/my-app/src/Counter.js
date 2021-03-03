2784562ddf878e634f4a0d688563d4c6c093c877
Waldos Mendoza <mendozawaldos@hotmail.com>
2021-03-02 20:10:04 -0600

    agregue un counter y quite codigo de app

---
+++ 04_Kata_frontend/02_React/my-app/src/Counter.js
@@ -0,0 +1,26 @@
+// El componente siempre inicia con mayusculas
+// Importar React
+// El componente y el archivo se deben llamar igual
+// Exportar el componente
+
+import React from 'react';
+
+class Counter extends React.Component {
+
+    constructor(props){
+        // El constructor recibe como argumento las propieades -> props
+        // Atributos asignados cuando llamamos el componente en un componente padre 
+        super(props);
+    }
+
+    render(){
+        return (
+            <>
+            <h1>Hola es mi contador</h1>
+            </>
+        )
+    }
+
+}
+
+export default Counter;
