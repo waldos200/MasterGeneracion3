520e019ef44db54e9788d729858b1130a283e639
Waldo Mendoza <mendozawaldos@hotmail.com>
2021-04-12 20:47:54 -0600

    viendo un poco de hooks y poniendo react-roter-dom

--- 04_Kata_frontend/02_React/todo-app/src/App.js
+++ 04_Kata_frontend/02_React/todo-app/src/App.js
@@ -1,13 +1,34 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
 import './App.css';
+import HolaMundo from './components/HolaClass';
 import HolaMundoClass from './components/HolaMundo';
+
+const NotFound = () => {
+  return <h2> Not Found </h2>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <HolaMundoClass/>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Switch>
+            <Route exact path="/">
+              <HolaMundo texto="Hola soy un stateless component"/>
+            </Route>
+            <Route exact path="/class" component={HolaMundoClass}/>
+            <Route exact path="/notFound" render={()=><NotFound/>}/>
+            <Redirect push to="/otFound" />
+          </Switch>
+        </header>
+      </div>
+    </Router>
   );
 }
 
