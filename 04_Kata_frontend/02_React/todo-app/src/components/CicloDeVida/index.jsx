89b80050cde9f1d5a9bee11a8964334b99d70d9d
Waldo Mendoza <mendozawaldos@hotmail.com>
2021-04-13 19:56:39 -0600

    creando ciclos de vida

---
+++ 04_Kata_frontend/02_React/todo-app/src/components/CicloDeVida/index.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+class Reloj extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    componentWillUnmount() {
+        console.log(4, 'El componente ha sido eliminado del DOM');
+    }
+
+    render() {
+        return <h3> {this.props.hora} </h3>
+    }
+}
+
+class CicloDeVida extends Component {
+    constructor(props){
+        super(props);
+        console.log(0, 'El componente se inicializa, aun no esta en el DOM');
+        this.state = {
+            hora: new Date().toLocaleTimeString()
+        }
+    }
+
+    componentDidMount() {
+        console.log(2, 'Componente ya mntado en el DOM');
+        setTimeout(() => {
+            this.setState({
+                hora: new Date().toLocaleTimeString()
+            })
+        }, 3000)
+    }
+
+    componentDidUpdate(prevPorps, prevState){
+        console.log(3, 'El estado o las props han cambiado');
+        if (this.state.hora !== prevState.hora){
+            setTimeout(() => {
+                this.setState({
+                    hora: new Date().toLocaleTimeString()
+                })
+            }, 1000)
+        }
+    }
+
+    render() {
+        console.log(1, 'El componente se dibua en el DOM');
+        console.log(this.state.hora);
+        return (
+            <>
+                <h2>
+                    Ciclo de Vida
+                </h2>
+                <h3>
+                    Hora actual: <Reloj hora={this.state.hora}/>
+                </h3>
+            </>
+        );
+    }
+}
+
+export default CicloDeVida;
