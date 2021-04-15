867b2536825c987d3ddf7867b6ff2688a615e9b8
Waldo Mendoza <mendozawaldos@hotmail.com>
2021-04-14 20:00:53 -0600

    agregue el componente click

--- 04_Kata_frontend/02_React/todo-app/src/components/Counter/index.jsx
+++ 04_Kata_frontend/02_React/todo-app/src/components/Counter/index.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const CounterHook = () => {
     const [ number, setNumber ] = useState(0)
+    const [ click, setClick] = useState(0)
+
+    useEffect( () => {
+        document.title = `Le has picado ${click} veces`
+    })
+
+    const sumar = () => {
+        setNumber(number + 1)
+        setClick(click + 1)
+    }
+
+    const restar = () => {
+        setNumber(number - 1)
+        setClick(click + 1)
+    }
+
+    const reiniciar = () => {
+        setNumber(0)
+        setClick(click + 1)
+    }
 
     return (
         <>
             <h1>Contador usando Hooks</h1>
-            <h3> { number } </h3>
-            <button onClick={ () => setNumber( number + 1 ) } > Sumas </button>
-            <button onClick={ () => setNumber( number - 1 ) } > Restar </button>
-            <button onClick={ () => { setNumber(0) } } > Reiniciar </button>
+            <h4>Le has picado {click} veces</h4>
+            <h3> { number === 0 ? 'No tienes cuenta' : number } </h3>
+            <button onClick={ sumar } > Sumas </button>
+            <button onClick={ restar } > Restar </button>
+            <button onClick={ reiniciar } > Reiniciar </button>
         </>
     );
     
