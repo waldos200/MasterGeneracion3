47ac4cbe32f91c1add2cc56330a059f3651f9cf4
Waldo Mendoza <mendozawaldos@hotmail.com>
2021-04-14 20:11:26 -0600

    CharacterHook

---
+++ 04_Kata_frontend/02_React/todo-app/src/components/CharactersHook/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CharacterHook extends Component {
+    render() {
+        return (
+            <>
+                
+            </>
+        );
+    }
+}
+
+export default CharacterHook;
