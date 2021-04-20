9d5a34a759a1fc301cef7930b5aa738475c88b46
Waldo Mendoza <mendozawaldos@hotmail.com>
2021-04-19 20:34:07 -0600

    Terminando Characters en Hooks

--- 04_Kata_frontend/02_React/todo-app/src/components/CharactersHook/index.jsx
+++ 04_Kata_frontend/02_React/todo-app/src/components/CharactersHook/index.jsx
@@ -1,13 +1,47 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 
-class CharacterHook extends Component {
-    render() {
-        return (
-            <>
-                
-            </>
-        );
+const Character = (character) => {
+    const {name, image} = character.character;
+
+    return(
+        <>
+            <h2>nombre: {name}</h2>
+            <img src= {image} alt="{name}"/>
+        </>
+    );
+}
+
+const CharacterHook = () => {
+    const [ characters, setCharacter ] = useState([])
+
+    const fetchApi = () => {
+        fetch(`https://rickandmortyapi.com/api/character/${Math.floor(Math.random() * 671)+1}`)
+            .then(response => response.json())
+            .then(data => setCharacter([...characters, data]));
     }
+
+    useEffect(() => {
+        fetchApi();
+    }, [])
+
+    useEffect(() => {
+        setTimeout( () => {
+            fetchApi();
+        },10000)
+    });
+
+    return (
+        <>
+            <h1>
+                Lista de personajes:
+            </h1> 
+            
+          
+            {characters.map(
+                (character, index) => <Character character={character} key={`charakter${index}`}/>
+            )}
+        </>
+    );
 }
 
 export default CharacterHook;
