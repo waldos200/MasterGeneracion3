510d67dc319a4ae7d78c3d064df7d337bec84b59
Waldo Mendoza <mendozawaldos@hotmail.com>
2021-04-27 19:33:14 -0600

    agregue la ruta de peticiones

--- 04_Kata_frontend/02_React/devflix/src/routes.js
+++ 04_Kata_frontend/02_React/devflix/src/routes.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './components/Home';
+import Peticiones from './components/Peticiones'
 
 const Routes = () => {
     return(
         <>
             <Route exact path="/">
                 <Home/>
+            </Route>
+            <Route exact path="/peticiones">
+                <Peticiones/>
             </Route>
         </>
     )
