f3e934f883e3ebfb07a8b70caf180e41200983b4
Waldo Mendoza <mendozawaldos@hotmail.com>
2021-06-29 19:34:08 -0600

    utilizando knet para poder usar una base de datos

---
+++ 06_Kata_backend/blog-api/data/migrations/20210630012446_users.js
@@ -0,0 +1,8 @@
+// Este archivo sirve como registro de los cambios de la BD
+exports.up = function(knex) {
+  // Aqui voy a escribr los camios que quiero hacer a mi BD
+};
+
+exports.down = function(knex) {
+  // Aqui voy a escribir el codigo que revierta los cambios de arriba
+};
