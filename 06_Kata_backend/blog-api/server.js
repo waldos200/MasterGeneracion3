24f65d721fb699692a6c89237aec489e573ff6c4
Waldo Mendoza <mendozawaldos@hotmail.com>
2021-06-30 19:48:35 -0600

    agregando body-parser a las rutas y creando las rutas

--- 06_Kata_backend/blog-api/server.js
+++ 06_Kata_backend/blog-api/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use('/api/v1', routes);
 
 const PORT = process.env.PORT || 9000;
 
