004634043bd69754733b75b2eddf016324f1e64a
Waldo Mendoza <mendozawaldos@hotmail.com>
2021-07-02 19:24:05 -0600

    Puse en accion celebrate

--- 06_Kata_backend/blog-api/server.js
+++ 06_Kata_backend/blog-api/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const { errors } = require('celebrate');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use('/api/v1', routes);
+app.use(errors());
 
 const PORT = process.env.PORT || 9000;
 
