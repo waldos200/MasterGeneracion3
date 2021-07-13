00660478b1c0e9b9238701bfc324cdefb2961ed9
Waldo Mendoza <mendozawaldos@hotmail.com>
2021-07-12 20:35:51 -0600

    validador de los datos

--- 06_Kata_backend/blog-api/src/routes/index.js
+++ 06_Kata_backend/blog-api/src/routes/index.js
@@ -1,46 +1,28 @@
 const express = require('express');
-const { celebrate, Joi, Segments } = require('celebrate');
 const router = express.Router();
 const UserController = require('../controllers/users');
 const PostController = require('../controllers/posts');
 const verify = require('../middlewares/verify');
+const { UserValidator, postCreateValidator } = require('../validators');
 
 // user routes
-const userCreateValidator = {
-    [Segments.BODY] : {
-        first_name: Joi.string().required(),
-        last_name: Joi.string().required(),
-        email: Joi.string().email().required(),
-        birth_date: Joi.date().required(),
-        gender: Joi.string().valid('male', 'female').required(),
-        password: Joi.string().required(),
-    } // Schema de validacion
-}
 
 router.get('/users', verify, UserController.fetch);
-router.get('/users/:id', verify, UserController.retrieve);
-router.post('/user/login', UserController.login);
-router.post('/users', celebrate(userCreateValidator), UserController.add);
-router.put('/users/:id', verify, celebrate(userCreateValidator), UserController.modify);
+router.get('/users/:id', verify, UserValidator.findOne, UserController.retrieve);
+router.post('/user/login', UserValidator.login, UserController.login);
+router.post('/users', UserValidator.create, UserController.add);
+router.put('/users/:id', verify, UserValidator.create, UserController.modify);
 router.delete('/users/:id', verify, UserController.eliminate);
 router.get('users/:id/posts', verify, UserController.populatedUser);
 
 module.exports = router;
 
 // posts routes
-const postCreateValidator = {
-    [Segments.BODY] : {
-        title: Joi.string().required(),
-        body: Joi.string().required(),
-        category: Joi.string().required(),
-        user_id: Joi.number().integer().required()
-    }
-}
 
 router.get('/posts/users', verify, PostController.populatedPosts);
 router.get('/posts', verify, PostController.fetch);
 router.get('/posts/:id', verify, PostController.retrieve);
-router.post('/posts', verify, celebrate(postCreateValidator), PostController.add);
+router.post('/posts', verify, postCreateValidator.create, PostController.add);
 router.put('/posts/:id', verify, PostController.modify);
 router.delete('/posts/:id', verify, PostController.eliminate);
 
